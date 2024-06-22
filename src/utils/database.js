184466a525359472a3b5bdc83c33616ad2d1524e
elmoatasem.madani@squadio.com <elmoatasem.madani@squadio.com>
2024-06-22 08:37:39 +0300

    Increasing database timout

--- src/utils/database.js
+++ src/utils/database.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000
         });
         console.log('MongoDB connected');
     } catch (err) {
