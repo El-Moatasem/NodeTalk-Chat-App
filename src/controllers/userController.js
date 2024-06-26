7f3e9af19670ca2ec6961fff10a80d5234f29a36
el.moatasem.mohamed@gmail.com <el.moatasem.mohamed@gmail.com>
2024-06-26 18:40:24 +0300

    Fixing Issues

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,20 +1,13 @@
+// src/controllers/userController.js
 const userService = require('../services/userService');
 const { publishEvent } = require('../services/eventBus');
-const { getCache, setCache } = require('../services/cacheService');
 
 exports.getUsers = async (req, res) => {
   try {
-    const cacheKey = 'users';
-    const cachedUsers = await getCache(cacheKey);
-
-    if (cachedUsers) {
-      return res.json(cachedUsers);
-    }
-
     const users = await userService.getUsers();
+    console.log("users: ", users)
     res.json(users);
     publishEvent('usersFetched', JSON.stringify(users));
-    setCache(cacheKey, users);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -22,22 +15,13 @@ exports.getUsers = async (req, res) => {
 
 exports.getUserById = async (req, res) => {
   try {
-    const userId = req.params.id;
-    const cacheKey = `user:${userId}`;
-    const cachedUser = await getCache(cacheKey);
-
-    if (cachedUser) {
-      return res.json(cachedUser);
-    }
-
-    const user = await userService.getUserById(userId);
+    const user = await userService.getUserById(req.params.id);
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
     res.json(user);
     publishEvent('userFetched', JSON.stringify(user));
-    setCache(cacheKey, user);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status (500).json({ error: error.message });
   }
 };
