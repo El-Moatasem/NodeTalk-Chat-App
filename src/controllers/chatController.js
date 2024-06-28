08d25b8415f5f505c67cc079096882d24afd2945
el.moatasem.mohamed@gmail.com <el.moatasem.mohamed@gmail.com>
2024-06-28 13:38:30 +0300

    Handling Cache

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -5,6 +5,8 @@ const Message = require('../models/message');
 const { publishEvent } = require('../services/eventBus');
 const { getCache, setCache, delCache } = require('../services/cacheService');
 
+global.invalidateCache = false;
+
 exports.createRoom = async (req, res) => {
   try {
     const { name, isPrivate, members } = req.body;
@@ -18,7 +20,6 @@ exports.createRoom = async (req, res) => {
     const chatRoom = await chatService.createRoom(name, isPrivate, members);
     res.status(201).json(chatRoom);
     publishEvent('chatRoomCreated', JSON.stringify(chatRoom));
-    await delCache('chatRooms');
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -30,7 +31,6 @@ exports.joinRoom = async (req, res) => {
     const chatRoom = await chatService.joinRoom(roomId, req.user._id);
     res.json(chatRoom);
     publishEvent('userJoinedRoom', JSON.stringify({ roomId, userId: req.user._id }));
-    await delCache('chatRooms');
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -42,7 +42,6 @@ exports.leaveRoom = async (req, res) => {
     const result = await chatService.leaveRoom(roomId, userId);
     res.json(result);
     publishEvent('userLeftRoom', JSON.stringify({ roomId, userId: req.user._id }));
-    await delCache('chatRooms');
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -55,7 +54,7 @@ exports.sendMessage = async (req, res) => {
     const populatedMessage = await chatService.sendMessage(roomId, sender, content);
     res.status(201).json(populatedMessage);
     publishEvent('messageSent', JSON.stringify(populatedMessage));
-    global.invalidateCache = true; // Invalidate cache
+    global.invalidateCache = true;
     await delCache(`messages:${roomId}`);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -81,7 +80,7 @@ exports.getMessages = async (req, res) => {
     res.json(messages);
     publishEvent('messagesFetched', JSON.stringify({ roomId, messages }));
     await setCache(cacheKey, messages);
-    global.invalidateCache = false; // Reset the flag after fetching messages
+    global.invalidateCache = false;
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -93,7 +92,7 @@ exports.editMessage = async (req, res) => {
     const message = await chatService.editMessage(messageId, req.user._id, newContent);
     res.json(message);
     publishEvent('messageEdited', JSON.stringify(message));
-    global.invalidateCache = true; // Invalidate cache
+    global.invalidateCache = true;
     await delCache(`messages:${message.roomId}`);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -106,7 +105,7 @@ exports.deleteMessage = async (req, res) => {
     const response = await chatService.deleteMessage(messageId, req.user._id);
     res.json(response);
     publishEvent('messageDeleted', JSON.stringify(response));
-    global.invalidateCache = true; // Invalidate cache
+    global.invalidateCache = true;
     await delCache(`messages:${response.roomId}`);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -132,7 +131,7 @@ exports.searchMessages = async (req, res) => {
     res.json(messages);
     publishEvent('messagesSearched', JSON.stringify({ roomId, keyword, messages }));
     await setCache(cacheKey, messages);
-    global.invalidateCache = false; // Reset the flag after fetching messages
+    global.invalidateCache = false;
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -140,19 +139,9 @@ exports.searchMessages = async (req, res) => {
 
 exports.getChatRooms = async (req, res) => {
   try {
-    const cacheKey = 'chatRooms';
-    const cachedChatRooms = await getCache(cacheKey);
-
-    if (cachedChatRooms) {
-      console.log('Cache hit for chatRooms');
-      return res.json(cachedChatRooms);
-    }
-
-    console.log('Cache miss for chatRooms');
     const chatRooms = await chatService.getChatRooms();
     res.json(chatRooms);
     publishEvent('chatRoomsFetched', JSON.stringify(chatRooms));
-    await setCache(cacheKey, chatRooms);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -160,19 +149,9 @@ exports.getChatRooms = async (req, res) => {
 
 exports.getPublicRooms = async (req, res) => {
   try {
-    const cacheKey = 'publicRooms';
-    const cachedRooms = await getCache(cacheKey);
-
-    if (cachedRooms) {
-      console.log('Cache hit for publicRooms');
-      return res.json(cachedRooms);
-    }
-
-    console.log('Cache miss for publicRooms');
     const rooms = await chatService.getPublicRooms();
     res.json(rooms);
     publishEvent('publicRoomsRetrieved', JSON.stringify(rooms));
-    await setCache(cacheKey, rooms);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -181,19 +160,9 @@ exports.getPublicRooms = async (req, res) => {
 exports.getPrivateRoomsForUser = async (req, res) => {
   try {
     const userId = req.user._id;
-    const cacheKey = `privateRooms:${userId}`;
-    const cachedRooms = await getCache(cacheKey);
-
-    if (cachedRooms) {
-      console.log('Cache hit for privateRooms:', userId);
-      return res.json(cachedRooms);
-    }
-
-    console.log('Cache miss for privateRooms:', userId);
     const rooms = await chatService.findPrivateRoomsForUser(userId);
     res.json(rooms);
     publishEvent('privateRoomsRetrieved', JSON.stringify(rooms));
-    await setCache(cacheKey, rooms);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -214,7 +183,6 @@ exports.findPrivateRoomByMembers = async (req, res) => {
     }
 
     publishEvent('privateRoomByMembersFound', JSON.stringify(room));
-    await delCache('chatRooms');
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -223,19 +191,9 @@ exports.findPrivateRoomByMembers = async (req, res) => {
 exports.getRoomMembers = async (req, res) => {
   try {
     const { roomId } = req.params;
-    const cacheKey = `roomMembers:${roomId}`;
-    const cachedMembers = await getCache(cacheKey);
-
-    if (cachedMembers) {
-      console.log('Cache hit for roomMembers:', roomId);
-      return res.json({ members: cachedMembers });
-    }
-
-    console.log('Cache miss for roomMembers:', roomId);
     const members = await chatService.getRoomMembers(roomId);
     res.json({ members });
     publishEvent('roomMembersRetrieved', JSON.stringify({ roomId, members }));
-    await setCache(cacheKey, members);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -244,19 +202,9 @@ exports.getRoomMembers = async (req, res) => {
 exports.getRoomInfo = async (req, res) => {
   try {
     const roomId = req.params.roomId;
-    const cacheKey = `roomInfo:${roomId}`;
-    const cachedRoom = await getCache(cacheKey);
-
-    if (cachedRoom) {
-      console.log('Cache hit for roomInfo:', roomId);
-      return res.json(cachedRoom);
-    }
-
-    console.log('Cache miss for roomInfo:', roomId);
     const room = await chatService.getRoomInfo(roomId);
     res.status(200).json(room);
     publishEvent('roomInfoRetrieved', JSON.stringify(room));
-    await setCache(cacheKey, room);
   } catch (error) {
     if (error.message === 'Room not found') {
       res.status(404).json({ message: error.message });
